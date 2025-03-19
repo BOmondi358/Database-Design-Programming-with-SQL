231850bb775ad6d12d4a19304027dd42a528e6a4
BOmondi358 <omondibrianne358@gmail.com>
2025-03-19 16:37:28 +0300

    Assignment: Introduction to Databases

---
+++ answers.sql
@@ -0,0 +1,9 @@
+-- Creating a new database named salesDB
+CREATE DATABASE salesDB;
+
+-- Dropping the database named demo if it exists
+DROP DATABASE IF EXISTS demo;
+
+/*CREATE DATABASE salesDB; → This command creates a new database named salesDB.
+DROP DATABASE IF EXISTS demo; → This command first checks if demo exists and deletes it if it does, preventing errors if the database does not exist.
+*/
